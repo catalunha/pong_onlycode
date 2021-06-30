7c8de2a18e5fbcf5f9d151015750d1eff9bf9dab
catalunha <catalunha@gmail.com>
2021-06-30 06:56:45 -0300

    updates

--- scripts/game_node2d.gd
+++ scripts/game_node2d.gd
@@ -9,36 +9,18 @@ var ball_speed = BALL_SPEED
 
 var screen_size
 var player_size
-var direction = Vector2(1.0,0.0)
+var ball_direction = Vector2(1.0,0.0)
+var ball_position
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	player_size = $PlayerLeftSprite.get_texture().get_size()
+	ball_position = $BallSprite.position
 
 
 func _process(delta):
-	print(direction)
-	var ball_pos = $BallSprite.position
-	var left_rect = Rect2($PlayerLeftSprite.position-player_size*0.5,player_size)
-	var right_rect = Rect2($PlayerRightSprite.position-player_size*0.5,player_size)
-	ball_pos += direction * ball_speed * delta
-	# Flip when touching roof or floor
-	if((ball_pos.y<0 and direction.y<0) or (ball_pos.y>screen_size.y and direction.y>0)):
-		direction.y = -direction.y
-	# Flip, change direction and increase speed when touching pads
-	if((left_rect.has_point(ball_pos) and direction.x<0) or (right_rect.has_point(ball_pos) and direction.x>0)):
-		direction.x = -direction.x
-		direction.y = randf() * 2.0 - 1
-		direction = direction.normalized()
-		ball_speed *= 1.1
-		
-	# Check gameover
-	if(ball_pos.x < 0 or ball_pos.x > screen_size.x):
-		ball_pos = screen_size * 0.5
-		ball_speed = BALL_SPEED
-		direction = Vector2(-1,0)
-		
-	$BallSprite.position = ball_pos
+
+	ball_position += ball_direction * ball_speed * delta
 	
 	# Move left pad
 	var player_left_position = $PlayerLeftSprite.position
@@ -47,6 +29,7 @@ func _process(delta):
 	if(player_left_position.y < screen_size.y and Input.is_action_pressed("left_move_down")):
 		player_left_position.y += PLAYER_SPEED * delta
 	$PlayerLeftSprite.position = player_left_position
+	
 	# Move right pad
 	var player_right_position = $PlayerRightSprite.position
 	if(player_right_position.y>0 and Input.is_action_pressed("right_move_up")):
@@ -54,6 +37,29 @@ func _process(delta):
 	if(player_right_position.y < screen_size.y and Input.is_action_pressed("right_move_down")):
 		player_right_position.y += PLAYER_SPEED * delta
 	$PlayerRightSprite.position = player_right_position
+
+	# Flip when touching roof or floor
+#	if((ball_position.y<0 and ball_direction.y<0) or (ball_position.y>screen_size.y and ball_direction.y>0)):
+	if((ball_position.y<0) or (ball_position.y>screen_size.y)):
+		ball_direction.y = -ball_direction.y
+
+	# Flip, change ball_direction and increase speed when touching pads
+	var player_left_rect2 = Rect2($PlayerLeftSprite.position-player_size*0.5,player_size)
+	var player_right_rect2 = Rect2($PlayerRightSprite.position-player_size*0.5,player_size)
+	if((player_left_rect2.has_point(ball_position) and ball_direction.x<0) or (player_right_rect2.has_point(ball_position) and ball_direction.x>0)):
+		ball_direction.x = -ball_direction.x
+		ball_direction.y = randf() * 2.0 - 1
+		ball_direction = ball_direction.normalized()
+		ball_speed *= 1.1
+		
+	# Check gameover
+	if(ball_position.x < 0 or ball_position.x > screen_size.x):
+		ball_position = screen_size * 0.5
+		ball_speed = BALL_SPEED
+		ball_direction = Vector2(-1,0)
+		
+	$BallSprite.position = ball_position
+	
 	
 	
 		
